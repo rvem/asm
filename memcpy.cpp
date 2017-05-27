190f518b05866b7c9aeac2da2d89f965559e6784
rvem <rvembox@gmail.com>
2017-05-27 13:51:08 +0300

    hw 1 and 2

---
+++ memcpy.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <emmintrin.h>
+#include <ctime>
+
+void *memcpy_1(void *dst, void const *src, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        *((char *) dst + i) = *((char *) src + i);
+    }
+    return dst;
+}
+
+void *memcpy_8(void *dst, const void *src, size_t n) {
+    for (int i = 0; i < n; i++) {
+        size_t tmp;
+        __asm__ volatile("mov\t" "(%1), %0 \n"
+                "mov\t" "%0,(%2)\n"
+        :"=r"(tmp)
+        :"r"((const char *) src + i), "r"((char *) dst + i)
+        :"memory");
+    }
+    return dst;
+}
+
+void *memcpy_16unaligned(void *dst, const void *src, size_t n) {
+    for (size_t i = 0; i < n; i += 16) {
+        __m128i tmp;
+
+        __asm__ volatile("movdqu\t (%1), %0\n"
+                "movntdq\t %0, (%2)\n"
+        :"=x"(tmp)
+        :"r"((char *) src + i), "r"((char *) dst + i)
+        :"memory");
+    }
+    return dst;
+}
+
+size_t copy_unaligned(void *dst, const void *src, size_t n) {
+    size_t offset = 0;
+    while (((size_t) dst + offset) % 16 != 0) {
+        *((char *) dst + offset) = *((char *) src + offset);
+        offset++;
+    }
+    size_t tail = (n - offset) % 16;
+    for (size_t i = n - tail; i < n; i++) {
+        *((char *) dst + i) = *((char *) src + i);
+    }
+    return tail;
+}
+
+void *memcpy_16aligned(void *dst, const void *src, size_t n) {
+    size_t tail = copy_unaligned(dst, src, n);
+    for (size_t i = 0; i < n - tail; i += 16) {
+        __m128i tmp;
+        __asm__ volatile("movdqa\t (%1), %0\n"
+                "movntdq\t %0, (%2)\n"
+        :"=x"(tmp)
+        :"r"((char *) src + i), "r"((char *) dst + i)
+        :"memory");
+    }
+    return dst;
+}
+
+const int N = 1 << 30;
+
+int main() {
+    char *src = new char[N];
+    char *dst = new char[N];
+    std::clock_t start, time1 = 0, time2 = 0, time3 = 0, time4 = 0;
+    start = std::clock();
+    memcpy_1(dst, src, N);
+    time1 = std::clock() - start;
+    start = std::clock();
+    memcpy_8(dst, src, N);
+    time2 = std::clock() - start;
+    start = std::clock();
+    memcpy_16aligned(dst, src, N);
+    time3 = std::clock() - start;
+    memcpy_16unaligned(dst, src, N);
+    time4 = std::clock() - start;
+    std::cout << "memcpy_1 average:" << time1 << std::endl;
+    std::cout << "memcpy_8 average:" << time2 << std::endl;
+    std::cout << "memcpy_16aligned average:" << time3 << std::endl;
+    std::cout << "memcpy_16unaligned average:" << time4 << std::endl;
+    return 0;
+}
---
+++ wordcount.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <emmintrin.h>
+
+const __m128i SPACE_MASK = _mm_set_epi8(32, 32, 32, 32, 32, 32, 32, 32,
+                                        32, 32, 32, 32, 32, 32, 32, 32);
+const __m128i ONE_MASK = _mm_set_epi8(1, 1, 1, 1, 1, 1, 1, 1,
+                                      1, 1, 1, 1, 1, 1, 1, 1);
+
+size_t word_count_naive(std::string &str) {
+    size_t res = 0;
+    bool is_space = true;
+    for (size_t i = 0; i < str.size(); i++) {
+        if (str[i] != ' ' && is_space) {
+            res++;
+            is_space = false;
+        } else if (str[i] == ' ') {
+            is_space = true;
+        }
+    }
+    return res;
+}
+
+size_t calc_mask(__m128i mask) {
+    size_t res = 0;
+    for (size_t i = 0; i < 16; ++i) {
+        res += (int) *((uint8_t *) &mask + i);
+    }
+    return res;
+}
+
+size_t word_count(std::string &s) {
+    char const *a = s.c_str();
+    size_t n = s.length();
+    size_t ans = 0;
+    size_t ind = 0;
+    __m128i res_mask = _mm_setzero_si128();
+    bool is_space = false;
+    while ((size_t) (a + ind) % 16 != 0) {
+        if (is_space && (*a + ind) != ' ') {
+            ans++;
+        }
+        is_space = (*a + ind) == ' ';
+        ind++;
+    }
+    size_t tail = n - (n - ind) % 16 - 16;
+    for (size_t i = ind; i < tail; i += 16) {
+        __m128i mask = _mm_cmpeq_epi8(_mm_loadu_si128((__m128i *) (a + i)), SPACE_MASK);
+        __m128i shifted_mask = _mm_cmpeq_epi8(_mm_loadu_si128((__m128i *) (a + i - 1)), SPACE_MASK);
+        __m128i count_mask = _mm_and_si128(_mm_andnot_si128(shifted_mask, mask), ONE_MASK);
+        res_mask = _mm_add_epi8(res_mask, count_mask);
+    }
+    ans += calc_mask(res_mask);
+    is_space = a[ind - 1] != ' ';
+    for (size_t i = tail; i < n; i++) {
+        if (a[i] != ' ' && is_space) {
+            ans++;
+            is_space = false;
+        } else if (a[i] == ' ') {
+            is_space = true;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    std::string s = "adsad    asdsa sf s   adsad    asdsa sf s   adsad    "
+            "asdsa sf s          asdsa   as  w  w w w";
+    std::cout << word_count_naive(s) << std::endl;
+    std::cout << word_count(s) << std::endl;
+    return 0;
+}
