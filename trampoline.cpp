47642d9352cd5e3b34a93582db1fc7de90c263cf
rvem <rvembox@gmail.com>
2018-04-07 19:42:52 +0300

    rewrite trampoline with saving object address in register

--- trampoline.cpp
+++ trampoline.cpp
@@ -5,69 +5,6 @@
 #include <iostream>
 #include <cassert>
 
-
-template<typename ... Args>
-struct args;
-
-template<>
-struct args<> {
-    static const int INT = 0;
-    static const int SSE = 0;
-};
-
-template<typename T, typename ... Args>
-struct args<T, Args ...> {
-    static const int INT = args<Args ...>::INT + 1;
-    static const int SSE = args<Args ...>::SSE;
-};
-
-template<typename T, typename ... Args>
-struct args<T *, Args ...> {
-    static const int INT = args<Args ...>::INT + 1;
-    static const int SSE = args<Args ...>::SSE;
-};
-
-template<typename ... Args>
-struct args<char, Args ...> {
-    static const int INT = args<Args ...>::INT + 1;
-    static const int SSE = args<Args ...>::SSE;
-};
-
-template<typename ... Args>
-struct args<short, Args ...> {
-    static const int INT = args<Args ...>::INT + 1;
-    static const int SSE = args<Args ...>::SSE;
-};
-template<typename ... Args>
-struct args<int, Args ...> {
-    static const int INT = args<Args ...>::INT + 1;
-    static const int SSE = args<Args ...>::SSE;
-};
-
-template<typename ... Args>
-struct args<long, Args ...> {
-    static const int INT = args<Args ...>::INT + 1;
-    static const int SSE = args<Args ...>::SSE;
-};
-
-template<typename ... Args>
-struct args<long long, Args ...> {
-    static const int INT = args<Args ...>::INT + 1;
-    static const int SSE = args<Args ...>::SSE;
-};
-
-template<typename ... Args>
-struct args<double, Args ...> {
-    static const int INT = args<Args ...>::INT;
-    static const int SSE = args<Args ...>::SSE + 1;
-};
-
-template<typename ... Args>
-struct args<float, Args ...> {
-    static const int INT = args<Args ...>::INT;
-    static const int SSE = args<Args ...>::SSE + 1;
-};
-
 class slab_allocator {
 
     static const size_t TRAMPOLINE_SIZE = 64; //56 actually
@@ -108,39 +45,25 @@ template<typename R, typename... Args>
 struct trampoline<R(Args...)> {
     template<typename F>
     trampoline(F const &func)
-            : func_obj(new F(func)), caller(&do_call < F > ), deleter(do_delete < F > ) {
+            : func_obj(new F(func)), deleter(do_delete < F > ) {
         code = allocator.malloc();
         pcode = (unsigned char *) code;
-        int int_args = args<Args...>::INT;
-        if (int_args < 6) {
-            //shift registers
-            for (int i = int_args - 1; i >= 0; i--) {
-                for (int j = 0; j < 3; ++j) {
-                    *(pcode++) = register_shift[i][j];
-                }
-            }
-            *pcode++ = 0x48;
-            *pcode++ = 0x89;
-            *pcode++ = 0xfe;
-            // mov rdi, imm
-            *(pcode++) = 0x48;
-            *(pcode++) = 0xBF;
-            *(void **) pcode = func_obj;
-            pcode += 8;
 
-            //mov rax, imm
-            *(pcode++) = 0x48;
-            *(pcode++) = 0xB8;
-            *(void **) pcode = (void *) &do_call<F>;
-            pcode += 8;
+        //mov r12, func_obj
+        *(pcode++) = 0x49;
+        *(pcode++) = 0xBC;
+        *(void **) pcode = func_obj;
+        pcode += 8;
 
-            //jmp rax
-            *(pcode++) = 0xFF;
-            *(pcode++) = 0xD0;
-        } else {
-	    //todo	
-	}
-        pcode = nullptr;
+        //mov rax, &do_call
+        *(pcode++) = 0x48;
+        *(pcode++) = 0xB8;
+        *(void **) pcode = (void *) &do_call<F>;
+        pcode += 8;
+
+        //jmp rax
+        *(pcode++) = 0xFF;
+        *(pcode++) = 0xE0;
     }
 
     ~trampoline() {
@@ -151,7 +74,16 @@ struct trampoline<R(Args...)> {
     }
 
     template<typename F>
-    static R do_call(void *obj, Args... args) {
+    static R do_call(Args... args) {
+        void *obj;
+
+        __asm__ volatile (
+        "movq %%r12, %0\n"
+        :"=r"(obj)
+        :"r"(obj)
+        : "memory"
+        );
+
         return (*(F *) obj)(args...);
     }
 
@@ -167,13 +99,6 @@ struct trampoline<R(Args...)> {
     trampoline &operator=(const trampoline &other) = delete;
 
 private:
-    const unsigned char register_shift[5][3] = {
-            {0x48, 0x89, 0xFE}, //mov rsi, rdi
-            {0x48, 0x89, 0xF2}, //mov rdx, rsi
-            {0x48, 0x89, 0xD1}, //mov rcx, rdz
-            {0x49, 0x89, 0xC8}, //mov r8, rcx
-            {0x4D, 0x89, 0xC1}, //mov r9, r8
-    };
     void *func_obj;
     void *code;
     unsigned char *pcode;
